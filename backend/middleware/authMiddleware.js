2307b7f9aa242c66099019947a3902988fecbe9c
Nishant Doma Sawaimoon <nishantcoclover@gmail.com>
2025-11-27 00:17:28 +0530

    Refactor authMiddleware for improved readability

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -9,15 +9,9 @@ const protect = async (req, res, next) => {
     req.headers.authorization.startsWith('Bearer')
   ) {
     try {
-      // Get token from header
       token = req.headers.authorization.split(' ')[1];
-
-      // Verify token
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-      // Get user from the token
       req.user = await User.findById(decoded.id).select('-password');
-
       next();
     } catch (error) {
       console.error(error);
@@ -29,5 +23,6 @@ const protect = async (req, res, next) => {
     res.status(401).json({ message: 'Not authorized, no token' });
   }
 };
+
 
 module.exports = { protect };
