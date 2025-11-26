1267d0e5509eb1ab753375737c3b18354cea1318
Nishant Doma Sawaimoon <nishantcoclover@gmail.com>
2025-11-27 00:13:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,18 +4,14 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 const path = require('path');
 
-// Load env vars
 dotenv.config();
 
-// Connect to database
 connectDB();
 
 const app = express();
 
-// Body parser
 app.use(express.json());
 
-// Enable CORS
 app.use(cors());
 
 // Mount routers
@@ -26,11 +22,9 @@ app.use('/api/orders', require('./routes/orders'));
 // Serve Frontend Static Files
 app.use(express.static(path.join(__dirname, '../frontend')));
 
-// --- NEW: Safer Catch-all Route ---
-// We changed this from app.get('*') to app.use() to prevent the crash
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
 });
 
-// Export the app for Vercel
 module.exports = app;
+
